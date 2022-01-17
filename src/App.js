ad30452f59105bf81e2f1996d5870a0fb8a1da90
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2022-01-17 21:23:47 +0400

    feat: added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.less';
+import Signup from './features/Signup/Signup';
 import FillMyProfile from './pages/FillMyProfile/FillMyProfile';
 
 function App() {
   return (
     <div className='App'>
-      <FillMyProfile />
+      <Router>
+        <Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/fill-my-profile' element={<FillMyProfile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
