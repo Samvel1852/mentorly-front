9c59c32b6ae0dcca6d7e2723be31d08420d03cdb
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2022-01-20 11:07:36 +0400

    feat: added design in Login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,9 @@
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button } from 'antd';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import { setLocalStorage } from '../../helpers/localstorage';
-// import { setLocalStorage } from '../../helpers/localstorage';
+
+import styles from './Login.module.less';
 
 export default function Login() {
   const navigate = useNavigate();
@@ -24,79 +25,79 @@ export default function Login() {
   };
 
   return (
-    <Form
-      name='basic'
-      labelCol={{
-        span: 10,
-      }}
-      wrapperCol={{
-        span: 10,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete='off'
-    >
-      <Form.Item
-        wrapperCol={{
-          offset: 13,
-          span: 16,
+    <div className={styles.formContainer}>
+      <Form
+        name='basic'
+        labelCol={{
+          span: 10,
         }}
-      >
-        <h1>Login</h1>
-      </Form.Item>
-      <Form.Item
-        label='Email'
-        name='email'
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label='Password'
-        name='password'
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name='remember'
-        valuePropName='checked'
         wrapperCol={{
-          offset: 8,
-          span: 16,
+          span: 10,
         }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 13,
-          span: 16,
+        initialValues={{
+          remember: true,
         }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete='off'
       >
-        <Button type='primary' htmlType='submit'>
-          Login
-        </Button>
-        <Form.Item>
-          Or <Link to='/signup'>Sign Up</Link>
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <h1>Login</h1>
         </Form.Item>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label='Email'
+          name='email'
+          labelCol={{ span: 24 }}
+          wrapperCol={{ span: 24 }}
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label='Password'
+          name='password'
+          labelCol={{ span: 24 }}
+          wrapperCol={{ span: 24 }}
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type='primary' htmlType='submit'>
+            Login
+          </Button>
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 4,
+            span: 24,
+          }}
+        >
+          Don`t have an account <Link to='/signup'>Sign Up</Link>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
