160396c3bcff37c473d927cb99745b6b67170783
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2022-01-20 10:57:59 +0400

    feat: added styles in confirm email page

--- src/pages/Confirm/Confirm.jsx
+++ src/pages/Confirm/Confirm.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from 'react-router-dom';
 import { Form, Input, Button } from 'antd';
 import axios from 'axios';
 
+import styles from './Confirm.module.less';
+
 export default function Confirm() {
   const navigate = useNavigate();
 
@@ -20,52 +22,66 @@ export default function Confirm() {
   };
 
   return (
-    <Form
-      name='basic'
-      labelCol={{
-        span: 10,
-      }}
-      wrapperCol={{
-        span: 10,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete='off'
-    >
-      <Form.Item
-        wrapperCol={{
-          offset: 13,
-          span: 16,
+    <div className={styles.formContainer}>
+      <Form
+        name='basic'
+        labelCol={{
+          span: 10,
         }}
+        wrapperCol={{
+          span: 10,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete='off'
       >
-        <h1>Please confirm Your Email</h1>
-      </Form.Item>
-      <Form.Item
-        label='Verification code'
-        name='verificationCode'
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input style={{ width: '80%' }} />
-      </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            span: 16,
+            offset: 6,
+          }}
+        >
+          <h1>Confirm e-mail</h1>
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 2,
+          }}
+        >
+          <p>
+            We have sent a verification code to your e-mail. <br /> Please
+            insert the code to validate your e-mail address.
+          </p>
+        </Form.Item>
+        <Form.Item
+          label='Code'
+          name='verificationCode'
+          labelCol={{ span: 24 }}
+          wrapperCol={{ span: 100 }}
+          rules={[
+            {
+              required: true,
+              message: 'Please input messaged code!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        wrapperCol={{
-          offset: 13,
-          span: 16,
-        }}
-      >
-        <Button type='primary' htmlType='submit'>
-          Confirm
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          wrapperCol={{
+            offset: 10,
+            span: 16,
+          }}
+        >
+          <Button type='primary' htmlType='submit'>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
