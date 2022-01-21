1c9d31f8365054086c629a2e92f792da1c54b6aa
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2022-01-21 17:56:45 +0400

    feat: changes in axiosInstance

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { getLocalStorage } from './localStorage';
 
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000/',
+const { REACT_APP_MAIN_URL } = process.env;
+
+export const myAxios = axios.create({
+  baseURL: REACT_APP_MAIN_URL,
   headers: {
     Authorization: getLocalStorage('accessToken') || '',
   },
 });
-
-export default axiosInstance;
