80463d86a750ac8d99575f3876c4e023f45a1026
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2022-01-26 17:41:03 +0400

    refactor: fixed axiosInstance work

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+
 import { getLocalStorage } from './localStorage';
 
-const {REACT_APP_MAIN_URL} = process.env
+const { REACT_APP_MAIN_URL } = process.env
 
 export const myAxios = () => axios.create({
   baseURL: `${REACT_APP_MAIN_URL}`,
